e833c66f0da31cbce59d08b56a097d37f82da203
Ishpreet <ishpreetsingh2808@gmail.com>
2020-02-22 03:28:07 -0500

    Sudoku Colver Completed

--- controller/sudoku_controller.py
+++ controller/sudoku_controller.py
@@ -2,86 +2,95 @@
 class SudokuController:
 
     def __init__(self, grid):
-        self.row_col = [0, 0]
         self.grid = grid
+        self.current_row = 0
+        self.current_col = 0
 
-    def show_grid(self, grid):
+
+    def show_grid(self):
         for row in range(9):
             for col in range(9):
-                print(grid[row][col], end=" ")
+                print(self.grid[row][col], end=" ")
             print("\n")
 
-    def is_cell_empty(self, grid):
+
+    def is_cell_empty(self):
         for row in range(9):
             for col in range(9):
-                if grid[row][col] == 0:
-                    self.row_col[0] = row
-                    self.row_col[1] = col
+                if self.grid[row][col] == 0:
+                    self.current_row = row
+                    self.current_col = col
                     return True
         return False
 
-    def is_row_valid(self, grid, row, num):
+
+    def is_row_valid(self, row, num):
         for c in range(9):
-            if grid[row][c] == num:
-                return True
-        return False
+            if self.grid[row][c] == num:
+                return False
+        return True
 
-    def is_col_valid(self, grid, col, num):
+
+    def is_col_valid(self, col, num):
         for r in range(9):
-            if grid[r][col] == num:
-                return True
-        return False
+            if self.grid[r][col] == num:
+                return False
+        return True
 
-    def is_sub_grid_valid(self, grid, row, col, num):
+
+    def is_sub_grid_valid(self, row, col, num):
         for r in range(3):
             for c in range(3):
-                if grid[r + row][c + col] == num:
-                    return True
+                if self.grid[r + row][c + col] == num:
+                    return False
+        return True
 
-        return False
 
-    def is_cell_valid(self, grid, row, col, num):
-        return not self.is_row_valid(grid, row, num) and not self.is_col_valid(grid, col, num) and not self.is_sub_grid_valid(grid, row - row%3, col - col%3, num)
+    def is_cell_valid(self, row, col, num):
+        return self.is_row_valid(row, num) and self.is_col_valid(col, num) and self.is_sub_grid_valid(row - row % 3, col - col % 3, num)
 
-    def mark_cell(self, grid, row, col, num):
-        grid[row][col] = num
 
-    def unmark_cell(self, grid, row, col):
+    def mark_cell(self, row, col, num):
+        self.grid[row][col] = num
+
+
+    def unmark_cell(self, row, col):
         grid[row][col] = 0
 
-    def solve(self, grid):
 
-        if(not self.is_cell_empty(grid)):
+    def solve(self):
+
+        if(not self.is_cell_empty()):
             return True
 
-        row = self.row_col[0]
-        col = self.row_col[1]
+        row = self.current_row
+        col = self.current_col
+
         for num in range(1, 10):
-            if self.is_cell_valid(grid, row, col, num):
-                grid[row][col] = num
-                if self.solve(grid):
+            if self.is_cell_valid(row, col, num):
+                self.mark_cell(row, col, num)
+                if self.solve():
                     return True
-                grid[row][col] = 0
+                self.unmark_cell(row, col)
         return False
 
 
 if __name__ == "__main__":
     print("Insdie Sudoku Controller.py")
-    grid = [[0, 9, 0, 0, 0, 0, 8, 5, 3],
-            [0, 0, 0, 8, 0, 0, 0, 0, 4],
-            [0, 0, 8, 2, 0, 3, 0, 6, 9],
-            [5, 7, 4, 0, 0, 2, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 9, 0, 0, 6, 3, 7],
-            [9, 4, 0, 1, 0, 8, 5, 0, 0],
-            [7, 0, 0, 0, 0, 6, 0, 0, 0],
-            [6, 8, 2, 0, 0, 0, 0, 9, 0]]
+    grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+            [5, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 8, 7, 0, 0, 0, 0, 3, 1],
+            [0, 0, 3, 0, 1, 0, 0, 8, 0],
+            [9, 0, 0, 8, 6, 3, 0, 0, 5],
+            [0, 5, 0, 0, 9, 0, 6, 0, 0],
+            [1, 3, 0, 0, 0, 0, 2, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 7, 4],
+            [0, 0, 5, 2, 0, 6, 3, 0, 0]]
     sc = SudokuController(grid)
     print("Original Sudoku")
-    sc.show_grid(grid)
-    if sc.solve(grid):
-        print("Sudoku is: ")
-        sc.show_grid(grid)
+    sc.show_grid()
+    if sc.solve():
+        print("Solution: \n")
+        sc.show_grid()
     else:
         print("Solution doesn't exist!")
-        print("Current Grid: \n", sc.show_grid(grid))
